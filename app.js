e034ae8480f6bcb955559005ab0d56507a36f8ea
kiethohoai <kietho.060193@gmail.com>
2024-07-20 15:43:02 +0700

    007 Handling POST Requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// middleware
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(404)
@@ -14,7 +17,7 @@ const port = 3000;
 // });
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`),
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
 app.get('/api/v1/tours', (req, res) => {
@@ -25,6 +28,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+//todo
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 app.listen(port, () => {
