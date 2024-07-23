4b556435ce4ebf7c612514ef1b972283c51ee2a5
kiethohoai <kietho.060193@gmail.com>
2024-07-23 18:23:49 +0700

    009 Reading Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,30 +1,40 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    console.log('🚀CHECK  tours =', tours);
 
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  // const id = req.params.id * 1;
-
-  // const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
 };
 
 // TODO createTour
@@ -37,7 +47,7 @@ exports.createTour = async (req, res) => {
         tour: newTour,
       },
     });
-  } catch (error) {
+  } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: 'Invalid data sent',
