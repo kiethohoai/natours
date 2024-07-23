a31b7c7c0c6ce0e47324441e146e784d48fd348b
kiethohoai <kietho.060193@gmail.com>
2024-07-23 09:12:26 +0700

    007 Refactoring for MVC

---
+++ models/tourModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Defined Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price'],
+  },
+});
+
+// Create a modal
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
--- server.js
+++ server.js
@@ -14,42 +14,6 @@ mongoose
   })
   .then(() => console.log('DB Connection successfully'));
 
-// Defined Schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    require: [true, 'A tour must have a price'],
-  },
-});
-
-// Create a modal
-const Tour = mongoose.model('Tour', tourSchema);
-
-// Create document
-const testTour = new Tour({
-  name: 'Hai Van Pass',
-  rating: 4.9,
-  price: 100,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR :', err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
