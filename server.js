3b41e6f13b585005219c3ba04caddc02267050a2
kiethohoai <kietho.060193@gmail.com>
2024-07-23 07:31:38 +0700

    004 Creating a Simple Tour Model

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-const mongoose = require('mongoose');
-
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
+// / const { Schema } = mongoose;
 
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE;
-
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log('DB Connection successfully'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
